a71e3c6c935f11bdb1d2ec082d920cc2c11dc2e1
Erik Burton <erikburton1@gmail.com>
2022-01-04 19:45:06 -0500

    Added a helper view for comments and refactored show articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @article = Article.find(params [:article_id])
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
@@ -9,4 +9,4 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:commenter, :body)
     end
-  end 
+  end
